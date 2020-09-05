2f39c78b4edb34c4c3a83b1b631eeeccae399356
Tracey Phuong Ha <thao.ph.ha@gmail.com>
2020-09-06 08:12:37 +1000

    Bind change event to metadata panel

--- belly-button-biodiversity/static/js/app.js
+++ belly-button-biodiversity/static/js/app.js
@@ -2,40 +2,39 @@
 // The data from the JSON file is arbitrarily named importedData as the argument
 d3.json("../../samples.json").then((importedData) => {
 
-console.log(importedData);
+	console.log(importedData);
 
-var data = importedData;
+	var data = importedData;
 
-// Dynamically add test Subject ID No. to the dropdown menus
-var names = data.names;
+	// Dynamically add test Subject ID No. to the dropdown menus
+	var names = data.names;
 
-names.forEach((name) => {
-	d3.select("#selDataset").append("option").text(name);
-})
+	names.forEach((name) => {
+		d3.select("#selDataset").append("option").text(name);
+	})
 
+	// Initializes the page with default plots
+	function init() {
 
-// Initializes the page with a default plot
-function init() {
+		// Choose data for test ID No. 940 plotted as default
+		defaultDataset = data.samples.filter(sample => sample.id === "940")[0];
+		console.log(defaultDataset);
 
-	// Choose data for test ID No. 940 plotted as default
-	defaultDataset = data.samples.filter(sample => sample.id === "940")[0];
-	console.log(defaultDataset);
+		// Select all sample_values, otu_ids and otu_labels of the selected test ID
+		allSampleValuesDefault = defaultDataset.sample_values;
+		allOtuIdsDefault = defaultDataset.otu_ids;
+		allOtuLabelsDefault = defaultDataset.otu_labels;
 
-	// Select all sample_values, otu_ids and otu_labels of the selected test ID
-	allSampleValuesDefault = defaultDataset.sample_values;
-	allOtuIdsDefault = defaultDataset.otu_ids;
-	allOtuLabelsDefault = defaultDataset.otu_labels;
+		// Select the top 10 OTUs for the ID with their sample_values, otu_ids and otu_labels
+		sampleValuesDefault = allSampleValuesDefault.slice(0, 10).reverse();
+		otuIdsDefault = allOtuIdsDefault.slice(0, 10).reverse();
+		otuLabelsDefault = allOtuLabelsDefault.slice(0, 10).reverse();
 
-	// Select the top 10 OTUs for the ID with their sample_values, otu_ids and otu_labels
-	sampleValuesDefault = allSampleValuesDefault.slice(0, 10).reverse();
-	otuIdsDefault = allOtuIdsDefault.slice(0, 10).reverse();
-	otuLabelsDefault = allOtuLabelsDefault.slice(0, 10).reverse();
+		console.log(sampleValuesDefault);
+		console.log(otuIdsDefault);
+		console.log(otuLabelsDefault);
 
-	console.log(sampleValuesDefault);
-	console.log(otuIdsDefault);
-	console.log(otuLabelsDefault);
-
-	// BAR CHART
+		// BAR CHART
 		// Add trace for the default Data
 		var trace1 = {
 			x: sampleValuesDefault,
@@ -57,7 +56,7 @@ function init() {
 		// Render the plot to the div tag with id "bar"
 		Plotly.newPlot("bar", barData, barlayout);
 
-	// BUBBLE CHART
+		// BUBBLE CHART
 		var trace2 = {
 			x: allOtuIdsDefault,
 			y: allSampleValuesDefault,
@@ -78,64 +77,67 @@ function init() {
 		
 		Plotly.newPlot('bubble', bubbleData, bubbleLayout);
 
-	// // DEMOGRAPHIC INFO
-	// 	demoDefault = data.metadata.filter(sample => sample.id === 940)[0];
-	// 	console.log(demoDefault);
+		// DEMOGRAPHIC INFO
+		demoDefault = data.metadata.filter(sample => sample.id === 940)[0];
+		console.log(demoDefault);
 
-	// 	// Display each key-value pair from the metadata JSON object
+		// Display each key-value pair from the metadata JSON object
+		Object.entries(demoDefault).forEach(([key, value]) => d3.select("#sample-metadata").append("p").text(`${key}: ${value}`));
+
+		// ADVANCED CHALLENGE: GAUGE CHART
 		
-	// 	Object.entries(demoDefault).forEach(([key, value]) => d3.select("#sample-metadata").append("p").text(`${key}: ${value}`));
+	}
 
-}
+	init();
 
-init();
 
-// Call updateBar() when a change takes place to the DOM
-d3.selectAll("#selDataset").on("change", updatePlot);
+	// Call updateBar() when a change takes place to the DOM
+	d3.selectAll("#selDataset").on("change", updatePlot);
 
-// 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-// This function is called when a dropdown menu item is selected
-function updatePlot() {
+	// 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+	// This function is called when a dropdown menu item is selected
+	function updatePlot() {
 
-	// Use D3 to select the dropdown menu
-		var inputElement = d3.select("#selDataset");
+		// Use D3 to select the dropdown menu
+			var inputElement = d3.select("#selDataset");
 
-	// Assign the value of the dropdown menu option to a variable
-		var inputValue = inputElement.property("value");
+		// Assign the value of the dropdown menu option to a variable
+			var inputValue = inputElement.property("value");
+			console.log(inputValue);
 
-	// Filter the dataset based on inputValue ID
-		dataset = data.samples.filter(sample => sample.id === inputValue)[0];
-		console.log(dataset);
+		// Filter the dataset based on inputValue ID
+			dataset = data.samples.filter(sample => sample.id === inputValue)[0];
+			console.log(dataset);
 
-	// Select all sample_values, otu_ids and otu_labels of the selected test ID
-		allSampleValues = dataset.sample_values;
-		allOtuIds = dataset.otu_ids;
-		allOtuLabels = dataset.otu_labels;
+		// Select all sample_values, otu_ids and otu_labels of the selected test ID
+			allSampleValues = dataset.sample_values;
+			allOtuIds = dataset.otu_ids;
+			allOtuLabels = dataset.otu_labels;
 
-	// Select the top 10 OTUs for the ID with their sample_values, otu_ids and otu_labels
+		// Select the top 10 OTUs for the ID with their sample_values, otu_ids and otu_labels
 		top10Values = allSampleValues.slice(0, 10).reverse();
 		top10Ids = allOtuIds.slice(0, 10).reverse();
 		top10Labels = allOtuLabels.slice(0, 10).reverse();
 
-	// BAR CHART
+		// BAR CHART
 		Plotly.restyle("bar", "x", [top10Values]);
 		Plotly.restyle("bar", "y", [top10Ids.map(outId => `OTU ${outId}`)]);
 		Plotly.restyle("bar", "text", [top10Labels]);
 
-	// BUBBLE CHART
+		// BUBBLE CHART
 		Plotly.restyle('bubble', "x", [allOtuIds]);
 		Plotly.restyle('bubble', "y", [allSampleValues]);
 		Plotly.restyle('bubble', "text", [allOtuLabels]);
 		Plotly.restyle('bubble', "marker.color", [allOtuIds]);
 		Plotly.restyle('bubble', "marker.size", [allSampleValues]);
-	}
 
-	// DEMOGRAPHIC INFO
+		// DEMOGRAPHIC INFO
 		metainfo = data.metadata.filter(sample => sample.id == inputValue)[0];
 
-	// Clear out current contents in the panel
+		// Clear out current contents in the panel
 		d3.select("#sample-metadata").html("");
 
-	// Display each key-value pair from the metadata JSON object
-		Object.entries(metainfo).forEach(([key, value]) => d3.select("#sample-metadata").append("p").text(`${key}: ${value}`)); 
-})
+		// Display each key-value pair from the metadata JSON object
+		Object.entries(metainfo).forEach(([key, value]) => d3.select("#sample-metadata").append("p").text(`${key}: ${value}`));
+	}
+});
